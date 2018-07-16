77ebf53d156146b3c311a173610bebc133b45597
DESKTOP-SSERRFO\Connor <connorgrail@yahoo.com>
2018-07-16 18:50:03 -0400

    Audio files and Events update

--- 3DSideScroller/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ 3DSideScroller/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,13 +13,41 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID MUSIC_TEST = 1068347553U;
+        static const AkUniqueID CHARACTER_READY = 2631240918U;
+        static const AkUniqueID ENEMY_DEATH = 1205999388U;
+        static const AkUniqueID ENEMY_HIT = 1010055213U;
+        static const AkUniqueID GAME_OVER = 1432716332U;
+        static const AkUniqueID GAME_WIN = 3218375656U;
+        static const AkUniqueID LEVEL_MUSIC = 1244594577U;
+        static const AkUniqueID MENU_ACCEPT = 3429288859U;
+        static const AkUniqueID MENU_CANCEL = 3180909377U;
+        static const AkUniqueID MENU_PAUSE = 2170009975U;
+        static const AkUniqueID MENU_SCROLL = 2072673020U;
+        static const AkUniqueID MRSPINES_ATTACK = 2651857535U;
+        static const AkUniqueID MRSPINES_FOOTSTEPS = 3136346432U;
+        static const AkUniqueID OOZER_FOOTSTEPS = 187604770U;
+        static const AkUniqueID OOZER_HITGROUND = 837528085U;
+        static const AkUniqueID OOZER_HITPLAYER = 366571179U;
+        static const AkUniqueID OOZER_SHOOT = 3194464978U;
         static const AkUniqueID PLAYER_BLOCK = 1833036836U;
         static const AkUniqueID PLAYER_DAMAGE = 2074073782U;
+        static const AkUniqueID PLAYER_FOOTSTEPSLEFT = 2882779481U;
+        static const AkUniqueID PLAYER_FOOTSTEPSRIGHT = 269686156U;
+        static const AkUniqueID PLAYER_HEALTHPICKUP = 3542219815U;
         static const AkUniqueID PLAYER_HEAVYATTACK = 3816881566U;
         static const AkUniqueID PLAYER_JUMP = 1305133589U;
         static const AkUniqueID PLAYER_LIGHTATTACK = 751122599U;
+        static const AkUniqueID PLAYER_MAGICATTACK = 853366470U;
+        static const AkUniqueID PLAYER_POWERPICKUP = 1256435816U;
+        static const AkUniqueID SLICER_ATTACK = 4209431020U;
         static const AkUniqueID SLICER_FOOTSTEPS = 2298066109U;
+        static const AkUniqueID TANKER_FOOTSTEPS = 3661249112U;
+        static const AkUniqueID TANKER_HOOK = 3281482822U;
+        static const AkUniqueID TANKER_SHOCKWAVE = 951923072U;
+        static const AkUniqueID TANKER_SWIPE = 2563941659U;
+        static const AkUniqueID VEX_CHANTFINISH = 534424066U;
+        static const AkUniqueID VEX_CHANTWINDUP = 276150034U;
+        static const AkUniqueID VEX_FOOTSTEPS = 2904509426U;
     } // namespace EVENTS
 
     namespace BANKS
