b7a4b24c15de146cd19400aa8cd137ee041b266f
Logan Stewart <stewart.jlt@gmail.com>
2018-07-18 16:11:21 -0400

    PlayerIconSwap
    Can assign icons to players now

---
+++ 3DSideScroller/Assets/Scripts/PlayerIcon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerIcon : MonoBehaviour {
+
+    public Image playerIcon;
+    public Sprite[] playerIcons = new Sprite[4];
+
+    public void SetIcon(int i)
+    {
+        playerIcon.sprite = playerIcons[i];
+    }
+
+  
+}
