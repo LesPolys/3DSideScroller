e232dfeea17d6dd1db75cf82c61211633a033c8f
Logan Stewart <stewart.jlt@gmail.com>
2018-07-20 17:32:23 -0400

    BossCardTrigger

---
+++ 3DSideScroller/Assets/PlayTitleCard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayTitleCard : MonoBehaviour {
+
+    public Hatch bossHatch;
+    bool triggerOnce = false;
+    GameObject titleCard;
+
+    void Update()
+    {
+        if (bossHatch.hatchShut && !triggerOnce)
+        {
+            triggerOnce = true;
+           // titleCard.GetComponent<Animator>().Play("");
+            //PlayImageCard thing
+
+        }
+    }
+
+}
